1a54c6061a8a4289281a9eb2395573edcf29de80
Emo <emilti1986@gmail.com>
2015-10-01 21:21:48 +0300

    Databases Processing Xml homework task 12

---
+++ DataBases/03.Processing Xml/Processing XML/12.ExtractPricesForAlbumsFromEarlierThan5yLINQ/ExtractPricesForAlbumsFromEarlierThan5yLINQ.cs
@@ -0,0 +1,32 @@
+﻿namespace ProcessingXML
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Xml;
+    using System.Xml.Linq;
+    
+
+    public class ExtractPricesForAlbumsFromEarlierThan5yLINQ
+    {
+        public static void Main()
+        {
+            XDocument xmlDoc = XDocument.Load("../../../Catalogue.xml");            
+            // from title in xmlDoc.Descendants("title")
+           //  select (string)title;
+
+
+            var prices =
+            from x in xmlDoc.Descendants("album")
+            where int.Parse(x.Element("year-of-release").Value) > 2010
+            select x.Element("price");
+
+            foreach (var price in prices)
+            {
+                Console.WriteLine("price:" + price.Value);
+            }
+        }
+    }
+}
