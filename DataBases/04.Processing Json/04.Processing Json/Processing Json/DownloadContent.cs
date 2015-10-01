87dc5a02c8ef813a1ba72b1d5a5d58a225c28018
emilti <emilti1986@gmail.com>
2015-10-02 02:18:52 +0300

    Database 04.Processing JSON Homework Tasks 1- 3

---
+++ DataBases/04.Processing Json/04.Processing Json/Processing Json/DownloadContent.cs
@@ -0,0 +1,30 @@
+﻿namespace ProcessingJson
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Xml;
+
+    public class DownloadContent
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            var webClient = new WebClient();
+
+            // Task 2 Download data
+            var data = webClient.DownloadData("https://www.youtube.com/feeds/videos.xml?channel_id=UCLC-vbm7OWvpbqzXaoAMGGw");
+            
+            // Task 3 Parse the XML to JSON
+            var xmlContent = Encoding.UTF8.GetString(data);            
+            var doc = new XmlDocument();
+            doc.LoadXml(xmlContent);
+            var jsonContent = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented);
+            Console.WriteLine(jsonContent);
+        }
+    }
+}
