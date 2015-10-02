9ab813dea038dee0abc03ff919bc337337c437ae
emilti <emilti1986@gmail.com>
2015-10-03 02:52:47 +0300

    Databases Processing JSON homework Tasks 5 - 6

--- DataBases/04.Processing Json/04.Processing Json/Processing Json/DownloadContent.cs
+++ DataBases/04.Processing Json/04.Processing Json/Processing Json/DownloadContent.cs
@@ -1,7 +1,5 @@
 ﻿namespace ProcessingJson
 {
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Linq;
     using System;
     using System.Collections.Generic;
     using System.IO;
@@ -10,6 +8,8 @@
     using System.Text;
     using System.Threading.Tasks;
     using System.Xml;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
 
     public class DownloadContent
     {
@@ -19,26 +19,73 @@
             var webClient = new WebClient();
 
             // Task 2 Download data
-            var data = webClient.DownloadData("https://www.youtube.com/feeds/videos.xml?channel_id=UCLC-vbm7OWvpbqzXaoAMGGw");
+            var data = DownloadRSS(webClient);
 
             // Task 3 Parse the XML to JSON
             var xmlContent = Encoding.UTF8.GetString(data);
             var doc = new XmlDocument();
-            doc.LoadXml(xmlContent);
-            var jsonContent = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented);
-            
-            // File.WriteAllText("../../../RSSfeed.json", jsonContent);
-            // Console.WriteLine(jsonContent);      
+            var jsonContent = ParseXMLToJSON(xmlContent, doc);              
 
             // Task 4 Using LINQ-to-JSON select all the video titles and print the on the console
-            JObject rss = JObject.Parse(jsonContent);
-            var videoTtles =
+            JObject rssJSON = JObject.Parse(jsonContent);
+            PrintTitles(rssJSON);
+
+            // Task 5 Parse JSON to 
+            var videos = GetAllVideosFromJSON(rssJSON.ToString());
+
+            // Task 6 Generate HTML
+            var html = GenerateHtml(videos);
+            File.WriteAllText("../../videos.html", html.ToString(), Encoding.UTF8);            
+        }
+
+        private static object GenerateHtml(IEnumerable<Video> videos)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<html><title>Telerik Videos From RSS</title><body>");
+            foreach (var video in videos)
+            {
+                sb.AppendFormat("<h1><a href='{0}'>" + video.Title + "</a></h1>", video.Link.Href);                
+                sb.AppendFormat("<iframe id='player' type='text/html' width='640' height='390'src='http://www.youtube.com/embed/{0}?autoplay=1' frameborder='0'></iframe>", video.Id);             
+                sb.Append("</iframe>"); 
+            }
+
+            sb.Append("</body></html>");
+            return sb.ToString();
+        }
+
+        private static string ParseXMLToJSON(string xmlContent, XmlDocument doc)
+        {
+            doc.LoadXml(xmlContent);
+            var jsonContent = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented);
+
+            File.WriteAllText("../../../RSSfeed.json", jsonContent);
+            return jsonContent;
+        }
+
+        private static byte[] DownloadRSS(WebClient webClient)
+        {
+            var data = webClient.DownloadData("https://www.youtube.com/feeds/videos.xml?channel_id=UCLC-vbm7OWvpbqzXaoAMGGw");
+            return data;
+        }
+
+        private static void PrintTitles(JObject rss)
+        {
+            var videoTitles =
             from p in rss["feed"]["entry"]
-            select (string)p["title"];
-            foreach (var videoTitle in videoTtles)
+            select p["title"];
+
+            foreach (var videoTitle in videoTitles)
             {
                 Console.WriteLine(videoTitle);
             }
+        }
+
+        private static IEnumerable<Video> GetAllVideosFromJSON(string rssJSONFeed)
+        {
+            var jsonRSSObj = JObject.Parse(rssJSONFeed);
+            var extractedVideos = jsonRSSObj["feed"]["entry"].Select(v => JsonConvert.DeserializeObject<Video>(v.ToString()));
+
+            return extractedVideos;
         }
     }
 }
---
+++ DataBases/04.Processing Json/04.Processing Json/Processing Json/Video.cs
@@ -0,0 +1,22 @@
+﻿namespace ProcessingJson
+{    
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Xml.Xsl;
+    using Newtonsoft.Json;
+
+    public class Video
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("link")]
+        public Link Link { get; set; }
+
+        [JsonProperty("yt:videoId")]
+        public string Id { get; set; }              
+    }
+}
