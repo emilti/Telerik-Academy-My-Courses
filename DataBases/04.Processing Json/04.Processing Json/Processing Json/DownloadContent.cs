9c2b2038551a00a2f42e0c79cc0268787ad1cc24
Emo <emilti1986@gmail.com>
2015-10-02 12:29:20 +0300

    Databases 4.Processing JSON - Task 4

--- DataBases/04.Processing Json/04.Processing Json/Processing Json/DownloadContent.cs
+++ DataBases/04.Processing Json/04.Processing Json/Processing Json/DownloadContent.cs
@@ -1,8 +1,10 @@
 ﻿namespace ProcessingJson
 {
     using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Net;
     using System.Text;
@@ -18,13 +20,25 @@
 
             // Task 2 Download data
             var data = webClient.DownloadData("https://www.youtube.com/feeds/videos.xml?channel_id=UCLC-vbm7OWvpbqzXaoAMGGw");
-            
+
             // Task 3 Parse the XML to JSON
-            var xmlContent = Encoding.UTF8.GetString(data);            
+            var xmlContent = Encoding.UTF8.GetString(data);
             var doc = new XmlDocument();
             doc.LoadXml(xmlContent);
             var jsonContent = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented);
-            Console.WriteLine(jsonContent);
+            
+            // File.WriteAllText("../../../RSSfeed.json", jsonContent);
+            // Console.WriteLine(jsonContent);      
+
+            // Task 4 Using LINQ-to-JSON select all the video titles and print the on the console
+            JObject rss = JObject.Parse(jsonContent);
+            var videoTtles =
+            from p in rss["feed"]["entry"]
+            select (string)p["title"];
+            foreach (var videoTitle in videoTtles)
+            {
+                Console.WriteLine(videoTitle);
+            }
         }
     }
 }
