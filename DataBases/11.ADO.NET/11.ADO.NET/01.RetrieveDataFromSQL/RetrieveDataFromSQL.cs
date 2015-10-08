06bd6efd982e23704351e8bd00861a3686690cf5
emilti <emilti1986@gmail.com>
2015-10-08 12:45:45 +0300

    DataBases 11.ADO.NET homework Tasks 1 - 4

---
+++ DataBases/11.ADO.NET/11.ADO.NET/01.RetrieveDataFromSQL/RetrieveDataFromSQL.cs
@@ -0,0 +1,78 @@
+﻿namespace Adonet
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class RetrieveDataFromSQL
+    {
+        static void Main()
+        {
+            SqlConnection dbCon = 
+                new SqlConnection("Data Source=localhost; Integrated Security=SSPI;" +
+                                            "Initial Catalog=North");
+            dbCon.Open();
+            using (dbCon)
+            {
+                GetCategoriesCount(dbCon);
+                GetCategoriesNameAndDesription(dbCon);
+                GetCategoriesAndProducts(dbCon);
+            }
+        }
+
+        private static void GetCategoriesCount(SqlConnection dbCon)
+        {
+            SqlCommand commandCategoriesCount = new SqlCommand(
+                  "SELECT COUNT(*) FROM Categories", dbCon);
+            int categoriesCount = (int)commandCategoriesCount.ExecuteScalar();
+            Console.WriteLine("Task 1:");
+            Console.WriteLine("Categories count: {0} ", categoriesCount);
+            Console.WriteLine("");
+        }
+
+        private static void GetCategoriesNameAndDesription(SqlConnection dbCon)
+        {
+            SqlCommand commandCategoriesNameAndDescription = new SqlCommand(
+                "SELECT CategoryName, Description FROM Categories", dbCon);           
+            SqlDataReader reader = commandCategoriesNameAndDescription.ExecuteReader();
+            using (reader)
+            {
+                Console.WriteLine("Task 2:");
+                while (reader.Read())
+                {
+                    string categoryName = (string)reader["CategoryName"];
+                    string description = (string)reader["Description"];
+                    Console.WriteLine("{0} - {1}", categoryName, description);
+                }
+
+                Console.WriteLine("");
+            }        
+        }
+
+        private static void GetCategoriesAndProducts(SqlConnection dbCon)
+        {
+            SqlCommand commandCategoriesAndProducts = new SqlCommand(
+                "SELECT m.CategoryName, n.ProductName " +
+                "from Products n " +
+                "INNER JOIN Categories m " +
+                "on m.CategoryID = n.CategoryID "+
+                "ORDER BY m.CategoryID", dbCon);
+            SqlDataReader reader = commandCategoriesAndProducts.ExecuteReader();
+            using (reader)
+            {
+                Console.WriteLine("Task 3:");
+                while (reader.Read())
+                {
+                    string categoryName = (string)reader["CategoryName"];
+                    string productName = (string)reader["ProductName"];
+                    Console.WriteLine("{0} - {1}", categoryName, productName);
+                }
+
+                Console.WriteLine("");
+            }
+        }       
+    }
+}
---
+++ DataBases/11.ADO.NET/11.ADO.NET/04.AddingDataToSQL/AddingDataToSQL.cs
@@ -0,0 +1,46 @@
+﻿namespace Adonet
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    
+    public class AddingDataToSQL
+    {
+        public static void Main()
+        {
+            SqlConnection dbCon =
+               new SqlConnection("Data Source=localhost; Integrated Security=SSPI;" +
+                                           "Initial Catalog=North");
+            dbCon.Open();
+            using (dbCon)
+            {
+                AddRowToProducts("MyProduct", 3, 2, 10, 99, 1, 1, 3, 0, dbCon);
+            }
+        }
+
+        public static void AddRowToProducts(string productName, int supplierID, 
+            int categoryID, int quantityPerUnit, 
+            int unitPrice, int  unitsInStock,   
+            int unitsOnOrder, int reorderLevel,
+            int discontinued, SqlConnection dbCon)
+        {
+            SqlCommand commandInsertRow = new SqlCommand(
+           "INSERT INTO Products(ProductName, SupplierID, CategoryID, QuantityPerUnit, UnitPrice, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued ) " +
+           "VALUES (@ProductName, @SupplierID, @CategoryID, @QuantityPerUnit, @UnitPrice, @UnitsInStock, @UnitsOnOrder, @ReorderLevel, @Discontinued)", dbCon);
+            commandInsertRow.Parameters.AddWithValue("@ProductName", productName);
+            commandInsertRow.Parameters.AddWithValue("@SupplierID", supplierID);
+            commandInsertRow.Parameters.AddWithValue("@CategoryID", categoryID);
+            commandInsertRow.Parameters.AddWithValue("@QuantityPerUnit", quantityPerUnit);
+            commandInsertRow.Parameters.AddWithValue("@UnitPrice", unitPrice);
+            commandInsertRow.Parameters.AddWithValue("@UnitsInStock", unitsInStock);
+            commandInsertRow.Parameters.AddWithValue("@UnitsOnOrder", unitsOnOrder);
+            commandInsertRow.Parameters.AddWithValue("@ReorderLevel", reorderLevel);
+            commandInsertRow.Parameters.AddWithValue("@Discontinued", discontinued);
+            commandInsertRow.ExecuteNonQuery();
+        }
+          
+    }
+}
