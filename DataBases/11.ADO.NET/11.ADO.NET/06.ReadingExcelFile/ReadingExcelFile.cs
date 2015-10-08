912dc66d1d667eea61cad9d30fd4ef34e177d4b2
emilti <emilti1986@gmail.com>
2015-10-09 02:49:11 +0300

    Databases 11.ADO.NET Task - 6 (not finished)

---
+++ DataBases/11.ADO.NET/11.ADO.NET/06.ReadingExcelFile/ReadingExcelFile.cs
@@ -0,0 +1,56 @@
+﻿namespace Adonet
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.OleDb;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ReadingExcelFile
+    {
+        public static void Main()
+        {
+
+            DataSet ds = new DataSet();
+
+            string connectionString = GetConnectionString();
+
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                conn.Open();
+                OleDbCommand cmd = new OleDbCommand();
+                cmd.Connection = conn;
+
+                // Get all Sheets in Excel File
+                DataTable dtSheet = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                // Loop through all Sheets to get data
+                foreach (DataRow dr in dtSheet.Rows)
+                {
+                    string sheetName = dr["TABLE_NAME"].ToString();
+
+                    if (!sheetName.EndsWith("$"))
+                        continue;
+
+                    // Get all rows from the Sheet
+                    cmd.CommandText = "SELECT * FROM [" + sheetName + "]";
+
+                    DataTable dt = new DataTable();
+                    dt.TableName = sheetName;
+
+                    OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                    da.Fill(dt);
+
+                    ds.Tables.Add(dt);
+                }
+
+                cmd = null;
+                conn.Close();
+            }
+
+            return ds;
+        }
+    }
+}
