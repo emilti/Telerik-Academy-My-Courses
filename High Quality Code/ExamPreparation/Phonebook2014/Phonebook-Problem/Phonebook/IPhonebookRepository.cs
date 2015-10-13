28e1f7392c207e8f40d50cb2cc24b4003173b70f
emilti <emilti1986@gmail.com>
2015-10-13 12:22:59 +0300

    Some bugs find and bottleneck

---
+++ High Quality Code/ExamPreparation/Phonebook2014/Phonebook-Problem/Phonebook/IPhonebookRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Phonebook
+{
+    using System.Collections.Generic;
+    using Phonebook;
+    
+    public interface IPhonebookRepository
+    {
+        bool AddPhone(string name, IEnumerable<string> phoneNumbers);
+
+        int ChangePhone(string oldPhoneNumber, string newPhoneNumber);
+
+        Class1[] ListEntries(int startIndex, int count);
+    }
+}
