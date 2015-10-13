9f55095026bb97951c8a04a2bdeb2ff047d37fca
emilti <emilti1986@gmail.com>
2015-10-13 21:34:32 +0300

    Refactoring of the Main method

---
+++ High Quality Code/ExamPreparation/Phonebook2014/Phonebook-Problem/Phonebook/Processor.cs
@@ -0,0 +1,64 @@
+﻿namespace Phonebook
+{
+    using System;
+    using Phonebook.Commands;
+
+    public class Processor
+    {
+        public void ProcessCommands(ICommand add, ICommand change, ICommand list, ICommand remove)
+        {
+            while (true)
+            {
+                string data = Console.ReadLine();
+                if (data == "End" || data == null)
+                {
+                    // Error reading from console 
+                    break;
+                }
+
+                int i = data.IndexOf('(');
+                if (i == -1)
+                {
+                    Console.WriteLine("error!");
+                    Environment.Exit(0);
+                }
+
+                string commandInput = data.Substring(0, i);
+                if (!data.EndsWith(")"))
+                {
+                    throw new ArgumentException("Invalid input");
+                }
+
+                string s = data.Substring(
+                    i + 1,
+                    data.Length - i - 2);
+                string[] commandData = s.Split(',');
+                for (int j = 0; j < commandData.Length; j++)
+                {
+                    commandData[j] = commandData[j].Trim();
+                }
+
+                if (commandInput.StartsWith("AddPhone") && (commandData.Length >= 2))
+                {
+                    add.Execute(commandData);
+                }
+                else if (commandInput.StartsWith("ChangePhone") && (commandData.Length == 2))
+                {
+                    change.Execute(commandData);
+                }
+                else if (commandInput.StartsWith("RemovePhone") && (commandData.Length == 2))
+                {
+                    remove.Execute(commandData);
+                }
+                else if ((commandInput == "List") && (commandData.Length == 2))
+                {
+                    list.Execute(commandData);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
--- High Quality Code/ExamPreparation/Phonebook2014/Phonebook-Problem/Phonebook/Program.cs
+++ High Quality Code/ExamPreparation/Phonebook2014/Phonebook-Problem/Phonebook/Program.cs
@@ -6,7 +6,7 @@
     using Phonebook.Factories; 
     using Phonebook.Print;     
 
-    public class Class2
+    public class MainProgram
     { 
         public static void Main()
         {
@@ -18,59 +18,8 @@
             ICommand change = factory.MakeCommandChange();
             ICommand list = factory.MakeCommandList();
             ICommand remove = factory.MakeCommandRemove();
-            
-            while (true)
-            {
-                string data = Console.ReadLine();
-                if (data == "End" || data == null)
-                {
-                    // Error reading from console 
-                    break;
-                }
-
-                int i = data.IndexOf('(');
-                if (i == -1)
-                {
-                    Console.WriteLine("error!");
-                    Environment.Exit(0);
-                }
-
-                string commandInput = data.Substring(0, i);
-                if (!data.EndsWith(")"))
-                {
-                    Main();
-                }
-
-                string s = data.Substring(
-                    i + 1,
-                    data.Length - i - 2);
-                string[] commandData = s.Split(',');
-                for (int j = 0; j < commandData.Length; j++)
-                {
-                    commandData[j] = commandData[j].Trim();
-                }
-
-                if (commandInput.StartsWith("AddPhone") && (commandData.Length >= 2))
-                {
-                    add.Execute(commandData);
-                }
-                else if (commandInput.StartsWith("ChangePhone") && (commandData.Length == 2))
-                {
-                    change.Execute(commandData);
-                }
-                else if (commandInput.StartsWith("RemovePhone") && (commandData.Length == 2))
-                {
-                    remove.Execute(commandData);
-                }
-                else if ((commandInput == "List") && (commandData.Length == 2))
-                {                    
-                    list.Execute(commandData);
-                }
-                else
-                {
-                    continue;
-                }
-            }
-        }       
+            Processor processor = new Processor();
+            processor.ProcessCommands(add, change, list, remove);
+        }             
     }
 }
