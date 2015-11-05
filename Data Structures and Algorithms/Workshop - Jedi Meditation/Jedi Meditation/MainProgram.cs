f89f8abe46140fff4a973b6b73a8b8a1a32ab3c2
emilti <emilti1986@gmail.com>
2015-11-05 16:43:04 +0200

    Data Structures and Algorithms 04. Dictionaries, Hash-Tables and Sets and Workshop homework added

---
+++ Data Structures and Algorithms/Workshop - Jedi Meditation/Jedi Meditation/MainProgram.cs
@@ -0,0 +1,56 @@
+﻿namespace JediMeditation
+{
+    using System;
+    using System.Collections;
+    using System.Text;
+
+    public class MainProgram
+    {
+        public static void Main()
+        {
+            string length = Console.ReadLine();
+            string input = Console.ReadLine();
+            var jedis = input.Split(new char[] { ' '},StringSplitOptions.RemoveEmptyEntries);
+            Queue masters = new Queue();
+            Queue knights = new Queue();
+            Queue padawans = new Queue();
+
+            for (int i = 0; i < jedis.Length; i++)
+            {
+                if (jedis[i][0] == 'm')
+                {
+                    masters.Enqueue(jedis[i]);
+                }
+                else if(jedis[i][0] == 'k')
+                {
+                    knights.Enqueue(jedis[i]);                    
+                }
+                else if (jedis[i][0] == 'p')
+                {
+                    padawans.Enqueue(jedis[i]);
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < masters.Count; i++)
+            {
+                sb.Append(masters.Dequeue() + " ");
+                i--;
+            }
+
+            for (int i = 0; i < knights.Count; i++)
+            {
+                sb.Append(knights.Dequeue() + " ");
+                i--;
+            }
+
+            for (int i = 0; i < padawans.Count; i++)
+            {
+                sb.Append(padawans.Dequeue() + " ");
+                i--;
+            }
+
+            Console.WriteLine(sb.ToString().Trim());
+        }
+    }
+}
