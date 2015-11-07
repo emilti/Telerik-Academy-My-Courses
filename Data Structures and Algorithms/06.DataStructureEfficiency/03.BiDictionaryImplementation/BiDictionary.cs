2f253024276078b4652d838e45928a2caae43d77
Emil Tishinv <emilti>
2015-11-07 14:12:18 +0200

    Data Structures and Algorithms 06.DataStructureEfficiency Task 3 BiDictionary implemented

---
+++ Data Structures and Algorithms/06.DataStructureEfficiency/03.BiDictionaryImplementation/BiDictionary.cs
@@ -0,0 +1,63 @@
+﻿namespace BiDictionaryImplementation
+{
+    using System.Collections.Generic;
+    using Wintellect.PowerCollections;
+    using System.Linq;
+
+    /// <summary>
+    /// Implement a class BiDictionary<K1,K2,T> that allows adding triples {key1, key2, value} and 
+    /// fast search by key1, key2 or by both key1 and key2.
+    /// Note: multiple values can be stored for given key.
+    /// </summary>
+    /// <typeparam name="K1">First key</typeparam>
+    /// <typeparam name="K2">Second key</typeparam>
+    /// <typeparam name="T">Template value</typeparam>
+    public class BiDictionary<K1, K2, T>
+    {
+
+        private MultiDictionary<K1, T> keysFirst;
+        private MultiDictionary<K2, T> keysSecond;
+        private MultiDictionary<string, T> keysBoth;
+
+        public BiDictionary()
+        {
+            this.keysFirst = new MultiDictionary<K1, T>(true);
+            this.keysSecond = new MultiDictionary<K2, T>(true);
+            this.keysBoth = new MultiDictionary<string, T>(true);
+        }
+
+        public void Add(K1 keyFirst,K2 keySecond, T element)
+        {
+            keysFirst.Add(keyFirst, element);
+            keysSecond.Add(keySecond, element);
+            string keysConcatenated = keyFirst.ToString() + keySecond.ToString();
+            keysBoth.Add(keysConcatenated, element);
+        }
+
+        public void Remove(K1 keyFirst, K2 keySecond, T element)
+        {
+            keysFirst.Remove(keyFirst);
+            keysSecond.Remove(keySecond);
+            string keysConcatenated = keysFirst.ToString() + keysSecond.ToString();
+            keysBoth.Remove(keysConcatenated, element);
+        }
+
+        public T[] FindByFirstKey(K1 keyFirst)
+        {
+            var students = this.keysFirst[keyFirst].ToArray();
+            return students;
+        }
+
+        public T[] FindBySecondKey(K2 keySecond)
+        {
+            return this.keysSecond[keySecond].ToArray();
+        }
+
+        public T[] FindByBothKeys(K1 keyFirst, K2 keySecond)
+        {
+            string keysConcatenated = keyFirst.ToString() + keySecond.ToString();
+            var students = this.keysBoth[keysConcatenated].ToArray();
+            return students;
+        }
+    }
+}
---
+++ Data Structures and Algorithms/06.DataStructureEfficiency/03.BiDictionaryImplementation/MainProgram.cs
@@ -0,0 +1,80 @@
+﻿namespace BiDictionaryImplementation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class MainProgram
+    {
+        public const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        public static void Main()
+        {
+            BiDictionary<string, string, Student> students = new BiDictionary<string, string, Student>();
+            List<string> firstKeysTest = new List<string>();
+            List<string> secondKeysTest = new List<string>();
+            List<string> bothKeysTest = new List<string>();
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                var lengthFirstName = random.Next(2, 20);
+                string firstName = GetRandomString(lengthFirstName, random);
+                var lengthLastName = random.Next(2, 20);
+                string lastName = GetRandomString(lengthLastName, random);
+                Student student1 = new Student(firstName, lastName);
+                students.Add(student1.FirstName, student1.LastName, student1);
+                firstKeysTest.Add(student1.FirstName);
+                secondKeysTest.Add(student1.LastName);
+                bothKeysTest.Add(student1.FirstName + student1.LastName);
+                if (i % 2 == 0)
+                {
+                    Student student2 = new Student(firstName, lastName);
+                    students.Add(student2.FirstName, student2.LastName, student2);
+                    firstKeysTest.Add(student2.FirstName);
+                    secondKeysTest.Add(student2.LastName);
+                    bothKeysTest.Add(student2.FirstName + student2.LastName);
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                var searchedFirstNameIndex = random.Next(0, firstKeysTest.Count);
+                var studentsByFirstName = students.FindByFirstKey(firstKeysTest[searchedFirstNameIndex]);
+                Console.WriteLine("New Search");
+                Console.WriteLine("Students searched by first name:");
+                foreach (var st in studentsByFirstName)
+                {
+                    Console.WriteLine("Student:" + st.FirstName + " " + st.LastName);
+                }
+
+                var searchedLastNameIndex = random.Next(0, secondKeysTest.Count);
+                var studentsByLastName = students.FindBySecondKey(secondKeysTest[searchedLastNameIndex]);                
+                Console.WriteLine("Students searched by last name:");
+                foreach (var st in studentsByLastName)
+                {
+                    Console.WriteLine("Student:" + st.FirstName + " " + st.LastName);
+                }               
+                
+                var studentsByBothNames = students.FindByBothKeys(firstKeysTest[i], secondKeysTest[i]);
+                Console.WriteLine("Students searched by both names:");
+                foreach (var st in studentsByBothNames)
+                {
+                    Console.WriteLine("Student:" + st.FirstName + " " + st.LastName);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static string GetRandomString(int length, Random random)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                var letter = random.Next(0, Alphabet.Length);
+                sb.Append(Alphabet[letter]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
