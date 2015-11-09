e5789452b4a7323eec4c5a732e33602262445f9f
emilti <emilti1986@gmail.com>
2015-11-10 00:41:14 +0200

    Data Structures and Algorithms 07.Recursion homework tasks 1-4

---
+++ Data Structures and Algorithms/07.Recursion/02.CombinationsWithDuplicatesofKfromN-element/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Combinations
+{
+    private static int numberOfLoops;
+    private static int numberOfIterations;
+    private static int[] loops;
+
+    public static void Main()
+    {
+        Console.Write("N = ");
+        numberOfIterations = int.Parse(Console.ReadLine());
+        Console.Write("K = ");
+        numberOfLoops = int.Parse(Console.ReadLine());
+        loops = new int[numberOfLoops];
+        NestedLoops(0);
+    }
+
+    public static void NestedLoops(int currentLoop)
+    {
+        if (currentLoop == numberOfLoops)
+        {
+            Console.WriteLine(string.Join(", ", loops));
+            return;
+        }
+
+        for (int counter = 1; counter <= numberOfIterations; counter++)
+        {
+            loops[currentLoop] = counter;
+            NestedLoops(currentLoop + 1);
+        }
+    }
+}
